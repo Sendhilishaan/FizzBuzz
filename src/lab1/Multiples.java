7f67fb107fe906604f3a07ca521765df03b4b714
Sendhilishaan <ishaansendhil@gmail.com>
2025-09-08 12:41:40 -0400

    Wrote multiples function

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,20 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples(1, 2, 3);
+        System.out.println(count);
+    }
+
+    static int multiples(int n, int a,  int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisibleByThree = i % a == 0;
+            boolean divisibleByFive = i % b == 0;
+            if (divisibleByThree ||  divisibleByFive) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
